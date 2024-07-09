b106a7f64db1119218d3eedb901a9305b75444bf
Kbenjamin92 <kipplbenjamin@gmail.com>
2024-07-09 17:21:24 -0400

    Created root layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,38 @@ function App() {
   return (
     <>
     <Grid
-    templateAreas={`"header"
-                    "main"
-                    "footer"`}
+    templateAreas={{
+      base:`"header"
+            "aside"
+            "main"
+            "footer"`,
+      sm:`"header"
+          "main"
+          "footer",`,
+      md:`"header header"
+          "aside main"
+          "footer footer"`,
+      lg: `"header header"
+            "aside main"
+            "footer footer",`}}
     h='500px'
     gridTemplateRows='repeat(3, 1fr)'
-    gridTemplateColumns='repeat(1, 1fr)'
+    templateColumns={{
+      base:'repeat(1, 1fr)',
+      md: 'repeat(2, 1fr)'
+    }}
     gap={1}>
       <GridItem area={'header'}>
-        <h1>Hello React</h1>
+        <h1>Header</h1>
+      </GridItem>
+      <GridItem area={'aside'}>
+        <h1>Aside</h1>
       </GridItem>
       <GridItem area={'main'}>
-        <h1>Hello React</h1>
+        <h1>Main</h1>
       </GridItem>
       <GridItem area={'footer'}>
-        <h1>Hello React</h1>
+        <h1>Footer</h1>
       </GridItem>
 
     </Grid>
